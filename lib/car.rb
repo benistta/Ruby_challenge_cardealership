205f17619c298de5bc5477d5b8d01252fb258ece
Becky Nisttahuz <benistta@hotmail.com>
2023-02-22 17:19:46 -0700

    All tests pass for Car class

--- lib/car.rb
+++ lib/car.rb
@@ -1,10 +1,19 @@
 class Car 
-    attr_reader :make, :model, :monthly_payment, :loan_length
+    attr_reader :make, :model, :monthly_payment, :loan_length, :color
     def initialize(brand, monthly_payment, loan_length)
         @make = brand.split.first
         @model = brand.split.last
         @monthly_payment = monthly_payment
         @loan_length = loan_length
+        @color = nil
         # require 'pry'; binding.pry
+    end
+
+    def total_cost
+        loan_length * monthly_payment
+    end
+
+    def paint!(car_color)
+        @color = car_color
     end
 end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -13,6 +13,22 @@ RSpec.describe Car do
     expect(car.model).to eq("Mustang")
     expect(car.monthly_payment).to eq(1500)
     expect(car.loan_length).to eq(36)
-    # expect(car.total_cost).to eq(54000)
+    
+  end
+
+  it 'has a total cost' do
+    car = Car.new("Ford Mustang", 1500, 36)
+    expect(car.total_cost).to eq(54000)
+  end
+
+  it 'has color' do
+    car = Car.new("Ford Mustang", 1500, 36)
+    expect(car.color).to eq(nil)
+  end
+
+  it 'has been painted' do
+    car = Car.new("Ford Mustang", 1500, 36)
+    car.paint!(:blue)
+    expect(car.color).to eq(:blue)
   end
 end
