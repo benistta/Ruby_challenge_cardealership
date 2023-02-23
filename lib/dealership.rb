35da34bfbac29b672d149df8b56b957262098398
Becky Nisttahuz <benistta@hotmail.com>
2023-02-22 18:04:05 -0700

    Tests for fisrt part of iteration 3 pass

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -14,4 +14,27 @@ class Dealership
         @inventory << car
         # require 'pry'; binding.pry
     end
+
+    def has_inventory? 
+        false
+    end
+
+    # def cars_by_make(make)
+    #     result = []
+    #     @inventory.each do |car|
+    #         if car.make == make
+    #          result << car
+    #         end
+            
+    #     end
+    #     return result
+    # end
+
+    def cars_by_make(make)
+        @inventory.find_all do |car|
+            car.make == make
+
+                
+        end
+    end
 end
