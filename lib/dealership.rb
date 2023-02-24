1fa36ade05a73d15bab6a5a4e4911876a6645782
Becky Nisttahuz <benistta@hotmail.com>
2023-02-23 17:44:07 -0700

    Tests pass for first part of iteration 4

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -16,7 +16,11 @@ class Dealership
     end
 
     def has_inventory? 
-        false
+        if @inventory.count <= 0
+            false
+        elsif @inventory.count > 0
+            true
+        end
     end
 
     # def cars_by_make(make)
@@ -45,5 +49,19 @@ class Dealership
 
     def details
         {"address" => @address, "total_value" => total_value}
+    end
+
+    def average_price_of_car
+        a = (total_value/4).to_s
+        # require 'pry'; binding.pry
+        return a.reverse.gsub(/...(?=.)/,'\&,').reverse
+    end
+    
+    def cars_sorted_by_price
+        @inventory.sort_by do |car|
+            car.monthly_payment * car.loan_length
+            
+        # require 'pry'; binding.pry
+        end
     end
 end
